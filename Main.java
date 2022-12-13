998be93947fc9c870275e5bd7987525c5ad1ba65
kostasgk <33001843+kostasgk@users.noreply.github.com>
2022-12-14 01:02:22 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,18 @@
 import java.lang.Math.*;
 
-public class Main {
+public class Main{
+
     public static void main(String[] args){
         System.out.println(area(2));
 
     }
 
     public static double area(double radius){
-        double Area ;
+        double ar = radius*radius*Math.PI ;
         if (radius<0){
-            return "Invalid Value";
+            return "Invalid value";
         } else {
-            area = radius*radius*Math.PI;
-            System.out.println("Area = " + Area);
+            return ar;
+            System.out.println("Area = " + area);
         }
     }
-}
